f729b51d11be3e4aaedc49b4515bc0d108149632
thienpr <dotuanthiendz112@gmail.com>
2024-11-25 10:12:26 +0700

    Update login.php

--- view/login.php
+++ view/login.php
@@ -21,6 +21,38 @@
     </div>
 
     <div class="login-page">
+        <?php
+            if(isset($_SESSION['user'])){
+                extract($_SESSION['user']);
+                
+        ?>
+        <div class="mb-3" style="margin-top: 20px;">
+            <label style="font-size: 15px" for="exampleInputEmail1" class="tkmk form-label">Xin chào
+                <?php echo $user ?></label>
+        </div>
+        <div class="mb-3" style="font-family: Popspismedium, sans-serif;">
+            <a href="index.php?act=quenmk">Quên mật khẩu</a>
+        </div>
+        <div class="mb-3" style="font-family: Popspismedium, sans-serif;">
+            <a href="index.php?act=edit_taikhoan">Cập nhật tài khoản</a>
+        </div>
+        <?php
+            if ($role == 1) {
+        ?>
+        <div class="mb-3" style="font-family: Popspismedium, sans-serif;">
+            <a href="admin/index.php">Đăng nhập vào admin</a>
+        </div>
+        <?php
+        }
+        ?>
+        <div class="mb-3" style="font-family: Popspismedium, sans-serif;">
+            <a href="index.php?act=logout">Thoát</a>
+        </div>
+
+        <?php
+        }else{
+
+        ?>
         <h2 class="textdangnhap text-center text-uppercase">Đăng nhập tài khoản</h2>
         <div class="chuacotk d-flex justify-content-center">
             <h6 class="text-center">Bạn chưa có tài khoản ?</h6>
@@ -28,24 +60,25 @@
                 <h6 class="texta text-center">Đăng ký tại đây</h6>
             </a>
         </div>
-        <form>
+        <form action="index.php?act=login" method="POST">
             <div class="mb-3" style="margin-top: 20px;">
                 <label for="exampleInputEmail1" class="tkmk form-label">Email</label>
-                <input type="email" class="inputform form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    placeholder=" Email" required>
+                <input name="email" type="email" class="inputform form-control" id="exampleInputEmail1"
+                    aria-describedby="emailHelp" placeholder=" Email" required>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="tkmk form-label">Mật khẩu</label>
-                <input type="password" class="inputform form-control" id="exampleInputPassword1"
+                <input name="pass" type="password" class="inputform form-control" id="exampleInputPassword1"
                     placeholder=" Mật khẩu">
             </div>
             <div class="mb-3">
-                <a class="quenmk" href="#">Quên mật khẩu</a>
+                <a class="quenmk" href="index.php?act=quenmk">Quên mật khẩu</a>
             </div>
             <div class="button">
-                <button type="button" class="btn btn-dark">Đăng Nhập</button>
+                <input type="submit" class="btn btn-dark" name="dangnhap" value="Đăng Nhập"></input>
             </div>
         </form>
+        <?php }?>
     </div>
 
     <?php
