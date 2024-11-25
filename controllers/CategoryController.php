fa0666f9ba9ac289359dcd9dd7f4acf217946615
thienpr <dotuanthiendz112@gmail.com>
2024-11-25 10:15:15 +0700

    Update CategoryController.php

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -7,34 +7,7 @@ class CategoryController {
     public function __construct() {
         $this->category = new Category();
     }
-<?php
-require_once "model/Category.php";
 
-class CategoryController {
-    private $category;
-
-    public function __construct() {
-        $this->category = new Category();
-    }
-
-    public function index() {
-        $categories = $this->category->getAllCategories();
-        include "view/home.php";
-    }
-
-    public function create() {
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            $name = $_POST['name'];
-            if ($this->category->createCategory($name)) {
-                echo "Thêm danh mục thành công!";
-            } else {
-                echo "Lỗi khi thêm danh mục!";
-            }
-        }
-        include "view/create.php";
-    }
-}
-?>
     public function index() {
         $categories = $this->category->getAllCategories();
         include "view/home.php";
